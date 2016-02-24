4dff6a7b0fd4622c4d35a3c6e20b8ccbda81deeb
Craig Merchant <hyabusa@gmail.com>
2016-02-24 21:17:54 +0000

    Added files via upload

---
+++ GIFCreator.swift
@@ -0,0 +1,67 @@
+//
+//  GIFCreator.swift
+//  GIFCreator
+//
+//  Created by Craig Merchant on 24/02/2016.
+//  Copyright © 2016 Craig Merchant. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+import ImageIO
+
+class GIFCreator: NSObject {
+    
+    var fps: Double = 10
+    var loopCount: Int = 0
+    
+    // Setting global color map to false will use a lot less memory when finalizing
+    var globalColorMap: Bool = false
+    
+    private var images: [NSURL] = []
+    private var imageDestination : CGImageDestinationRef!
+    
+    init(imageURLS:[NSURL])
+    {
+        self.images = imageURLS
+    }
+    
+    func createGIF(destinationURL: NSURL) {
+
+        imageDestination = CGImageDestinationCreateWithURL(destinationURL, kUTTypeGIF, Int(images.count), nil)
+        CGImageDestinationSetProperties(imageDestination, gifProperties())
+        
+        for (_, imageURL) in images.enumerate()
+        {
+            autoreleasepool({
+                if let path = imageURL.path {
+                    if let img = UIImage(contentsOfFile: path) {
+                        if let cg = img.CGImage {
+                            let delay = 1 / fps
+                            let properties = frameProperties(delay)
+                            CGImageDestinationAddImage(imageDestination, cg, properties)
+                        }
+                    }
+                }
+            })
+        }
+        
+        CGImageDestinationFinalize(imageDestination)
+    }
+    
+    private func frameProperties(frameDelay:NSTimeInterval) -> CFDictionary
+    {
+        let dict : CFDictionary = [kCGImagePropertyGIFDelayTime as String :frameDelay]
+        return [kCGImagePropertyGIFDictionary as String :dict]
+    }
+    
+    private func gifProperties() -> CFDictionary
+    {
+        let dict : CFDictionary = [
+            kCGImagePropertyGIFLoopCount as String : 0,
+            kCGImagePropertyGIFHasGlobalColorMap as String: globalColorMap,
+            kCGImagePropertyGIFLoopCount as String: loopCount
+        ]
+        return [kCGImagePropertyGIFDictionary as String:dict]
+    }
+}
